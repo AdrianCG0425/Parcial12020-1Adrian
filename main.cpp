a7f8b4b0f679e1e15eb38ba7103ed20c6485b11f
AdrianCuervoG <adry0425@hotmail.com>
2020-10-17 17:30:10 -0500

    5. entrega final

--- main.cpp
+++ main.cpp
@@ -23,5 +23,21 @@ int main()
 }
 void ataques(float distancia,float Ho,float Hd,float an,float vel){
     float x1,y1,x2,y2,x3,y3; //  (x1,y1) tanque1  , (x2,y2) tanque2  , (x3, y3) donde deben estar las balas
+    float t,t2,t3,r2,r1;             // t = tiempo total que demora el proyectil   t2 tiempo necesario para que no le pegue
+    r1=0.05*distancia;
+    r2=0.025*distancia;
+    x2=distancia;
+    if(Ho>Hd)y2=0;
+    else if(Ho<Hd)y2=Hd-Ho;
+    else y2=0;
+    x1=0;
+    if(Ho>Hd)y1=Ho-Hd;
+    else if(Ho<Hd)y1=0;
+    else y1=0;
+    // ecuaciones de la bala oponente
+    t=x2/vel*cos(an);
+    t2=t-2.5;
+
+
 
 }
